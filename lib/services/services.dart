4b38a8adc6dff75b7d2fb6ce783637c687eb0452
Muhammad Rizky Syahputra <rizkyputra531@Rizky-Macbook.local>
2021-12-13 23:54:59 +0800

    User Services + API

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+import 'dart:convert';
+
 import 'package:food_market/models/models.dart';
+import 'package:http/http.dart' as http;
 
 part 'user_services.dart';
 part 'food_services.dart';
 part 'transaction_services.dart';
+
+String baseURL = "http://food_market_backend.test/api/";
